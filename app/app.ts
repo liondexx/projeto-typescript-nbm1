1e1dc64b5dbe5ed593dfc3e66a0e2bd7d920d6d0
lion patrocinio <lionfilesoffices@gmail.com>
2022-08-24 21:53:33 -0300

    dando funcionalidade ao codigo

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,8 @@
-import { Negociacao } from './models/negociacao.js';
+import { NegociacaoController } from "./controllers/negociacao-controler.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
-alert("oi")
+const controler = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controler.adiciona();
+});
